97e0e87e16724b1078c7bf98bfbfcd4fc43b2f41
robert.scovell <robert.scovell@metoffice.gov.uk>
2019-06-26 16:18:55 +0100

    Upgrade coefficient plotting scripts to Python3. Also tidied up these scripts and the compilation script.

--- CWT/comp.sh
+++ CWT/comp.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-LIBFFTW3DIR=~/local_install
+LIBFFTW3DIR=/usr
+BOOSTDIR=/usr
+HDF5DIR=/usr
 
-g++ -O3 -fopenmp -DBOOST_DISABLE_ASSERTS mexicanHatTransform.cc -o mexicanHatTransform -I"${LIBFFTW3DIR}/include" -L"${LIBFFTW3DIR}/lib" -lfftw3 -lhdf5 
-g++ -O3 -fopenmp -DBOOST_DISABLE_ASSERTS morletTransform.cc -o morletTransform  -I"${LIBFFTW3DIR}/include" -L"${LIBFFTW3DIR}/lib" -lfftw3 -lhdf5
+CPPFLAGS="-O3 -fopenmp -DBOOST_DISABLE_ASSERTS -I${BOOSTDIR}/include -I${LIBFFTW3DIR}/include -I${HDF5DIR}/include"
+LDFLAGS="-fopenmp -L${BOOSTDIR}/lib -L${LIBFFTW3DIR}/lib -lfftw3 -lhdf5"
+
+g++ ${CPPFLAGS} mexicanHatTransform.cc -o mexicanHatTransform ${LDFLAGS}
+g++ ${CPPFLAGS} morletTransform.cc -o morletTransform ${LDFLAGS} 
  
--- CWT/pltcoeffsMH.py
+++ CWT/pltcoeffsMH.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python2.7
-
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 # -----------------------------------------------------------------------------
 # (C) British Crown Copyright 2017-2019 Met Office.
 # All rights reserved.
@@ -31,20 +31,22 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+import sys
 import h5py
 import numpy as np
+import matplotlib as mpl
+mpl.rcParams['image.interpolation']='nearest'
+mpl.rcParams['font.size'] = 10
 import matplotlib.pyplot as plt
 import matplotlib.colors
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from matplotlib.ticker import MultipleLocator
-import sys
-import viridis
 
 h5FH1=h5py.File(sys.argv[1])
 
 coeffs=np.copy(h5FH1['coefficients'])
 
-# Dataset {34/34, 1/1, 1024/1024, 1024/1024, 2/2}
+# Dataset dimensions #scales x #rotations x #y-pixels x #x-pixels x 2. Example: (34,1,1024,1024,2)
 
 def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
     new_cmap = matplotlib.colors.LinearSegmentedColormap.from_list(
@@ -54,45 +56,49 @@ def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
 
 scales=[0,1,2,3,4,5,6,7,8,9,10,11]
 
-fig,axes=plt.subplots(3,2)
-ax=axes.flat[0]
-
-imCount=0
+imCount=1
 # Display real values at selected scales
 for i in range(2,7,2):
+    plt.subplot(3,2,imCount)
     iScale = scales[i]
     s=np.power(2.0,iScale)
     lambd=s*(2.0*np.pi)/np.sqrt(2.0+0.5)
-    imArr=np.copy(coeffs[iScale,iOri,:,:,0])
-    ax=axes.flat[imCount]
+    imArr=np.copy(coeffs[iScale,0,:,:,0])
     minIm=np.min(imArr)
     maxIm=np.max(imArr)
     maxAbs=max(minIm,maxIm)
-    im=ax.imshow(imArr,cmap=plt.get_cmap('RdBu_r'),interpolation='nearest',vmin=-maxAbs,vmax=maxAbs)#,vmin=-80.0,vmax=80.0)
-    ax.set_title("$W_{\psi,j}\,[a=%2.1f,\lambda=%3.1f]}$" % (s,lambd) )
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
+    im=plt.imshow(imArr,cmap=plt.get_cmap('RdBu_r'),interpolation='nearest',vmin=-maxAbs,vmax=maxAbs)
+    ax=plt.gca()
+    if imCount==2:
+        plt.title(u'$W_{\psi,j}$')
+    plt.xticks([])
+    plt.yticks([])
     div=make_axes_locatable(ax)
     cax=div.append_axes("right",size="10%",pad=0.05)
     plt.colorbar(im,cax=cax)
     imCount+=2
-imCount=1
+
 # Display absolute values
+imCount=2
 for i in range(2,7,2):
+    plt.subplot(3,2,imCount)
     iScale=scales[i]
     s=np.power(2.0,iScale)
     lambd=s*(2.0*np.pi)/np.sqrt(2.0+0.5)
-    imArr=np.copy(coeffs[iScale,iOri,:,:,0])
-    ax=axes.flat[imCount]
+    imArr=np.copy(coeffs[iScale,0,:,:,0])
+#    ax=axes.flat[imCount]
     minIm=np.min(imArr)
     maxIm=np.max(imArr)
     maxAbs=max(minIm,maxIm)
     imd=np.absolute(np.square(imArr))
     imd[imd<0.125]=0.125
-    im=ax.imshow(np.log2(imd),cmap=plt.get_cmap('viridis'),interpolation='nearest')#,vmin=-80.0,vmax=80.0)
-    ax.set_title("$\log_{2}|W_{\psi,j}|^2\,[a=%2.1f,\lambda=%3.1f]}$" % (s,lambd) )
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
+    im=plt.imshow(np.log2(imd),cmap=plt.get_cmap('viridis'),interpolation='nearest')
+    if imCount==1:
+        plt.title(u'$\log_{2}|W_{\psi,j}|^2$')
+    plt.ylabel("$a=%2.1f$" % (s),rotation=90,size='large')
+    ax=plt.gca()
+    plt.xticks([])
+    plt.yticks([])
     div=make_axes_locatable(ax)
     cax=div.append_axes("right",size="10%",pad=0.05)
     plt.colorbar(im,cax=cax)
