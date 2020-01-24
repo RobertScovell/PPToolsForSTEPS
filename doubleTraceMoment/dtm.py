241b78a390b8de11ebe2290e91709dc0b45c4cab
robert.scovell <robert.scovell@metoffice.gov.uk>
2020-01-24 10:04:14 +0000

    Fixed some issues with DTM code. Now seems to work accurately on synthetic fields.

--- doubleTraceMoment/dtm.py
+++ doubleTraceMoment/dtm.py
@@ -38,100 +38,108 @@ import matplotlib as mpl
 mpl.rcParams['image.interpolation']='nearest'
 mpl.rcParams['font.size'] = 14
 import matplotlib.pyplot as plt
-import matplotlib.colors
-from mpl_toolkits.axes_grid1 import make_axes_locatable
 import sys
-import scipy.ndimage
-from scipy.signal import convolve2d
-
+import pywt
 import fracInt
 
 def pyramid(image,nLevs):
     outArr=[]
+    cA=image
     for iLvl in range(nLevs):
-        filtLen=2**iLvl
-        box=np.ones((filtLen,filtLen))
-        # Convolve with box of size 2**iLvl
-        out=scipy.signal.fftconvolve(image,box,mode='valid')
-        # Pick only disjoint boxes
-        out=out[0::filtLen,0::filtLen]
-        outArr.append(out)
+        cA,(cH,cV,cD)=pywt.dwt2(cA,'haar')
+        fac=1.#(2**iLvl)
+        outArr.append(np.absolute(fac*cA)/np.mean(fac*cA))
     return outArr
 
+def dtm(data,minScale=1,maxScale=10,etaMin=-2.,etaMax=1.,qVals=[1.5,2.0,2.5,3.0],debug=False):
+    # Determine the number of decomposition levels needed
+    n0=data.shape[0]
+    n1=data.shape[1]
+    nLevs=int(np.floor(np.log2(np.min((n0,n1)))))
+    
+    # Compute fluxes as pyramid
+    imPyramid=pyramid(data,nLevs)
+    
+    D=2.
+    nScales=maxScale-minScale+1
+    
+#    # Flux at shortest scale considered
+#    epsLambda0=imPyramid[minScale]
+#    epsLambda0[epsLambda0<1.0e-16]=1.0e-16
+    lambda0=np.power(2.,maxScale)/np.power(2.,minScale)
+    
+    etaVals=np.exp(np.linspace(etaMin,etaMax,num=31,endpoint=True))
+    if debug==True:
+        print("log(eta):",np.log(etaVals))
+        print("q:",qVals)
+    
+    tr=np.empty((len(qVals),len(etaVals),nScales))
+    kQEta=np.empty((len(qVals),len(etaVals)))
+    
+    for (iEta,eta) in enumerate(etaVals):
+        lambdas=[]
+    
+        # Compute trace moments (as in Single Trace Moment method) but on on epsEtas, for a range of q and scale (for this value of eta)
+        for iLvl in range(nScales):
+            lambdaLvl=np.power(2.,maxScale)/np.power(2.,minScale+iLvl)
+            lambdas.append(lambdaLvl)
+            for (iq,q) in enumerate(qVals):
+                imPyramid[iLvl][imPyramid[iLvl]<1.0e-16]=1.0e-16
+                tr[iq,iEta,iLvl]=np.mean(np.power(imPyramid[iLvl],eta*q))/np.power(np.mean(np.power(imPyramid[iLvl],eta)),q)
 
-# Load data
-data=np.genfromtxt(sys.argv[1],delimiter=",")
-print("Mean:",np.mean(data))
+        # Now estimate K(q,eta) from slopes of plots of log(Tr_lambda[epsilon_lamda^q]) against log(lambda)
+        for iq,q in enumerate(qVals):
+            (m,c)=np.polyfit(np.log2(lambdas[:]),np.log2(tr[iq,iEta,:]),1)
+            kQEta[iq,iEta]=m#+(q-1.)*D
+    
+    # Evaluate the the slopes of log(K(q,eta)) vs log(eta) for fixed qs to get alpha
+    umfParamEsts=[]
+    for (iq,q) in enumerate(qVals):
+        alphaC1Ests=[]
+        hw=10
+        for i in range(hw,len(etaVals)-hw):
+            alphaC1Ests.append(np.polyfit(np.log(etaVals[i-hw:i+hw]),np.log(np.absolute(kQEta[iq,i-hw:i+hw])),1))
+        iEst=np.argmax([x[0] for x in alphaC1Ests])
+        alphaEst=alphaC1Ests[iEst][0]
+        intercept=alphaC1Ests[iEst][1]
+        if debug == True:
+            plt.plot(np.log(etaVals),np.log(np.absolute(kQEta[iq,:])))
+            plt.plot(np.log(etaVals),alphaEst*np.log(etaVals)+intercept)
+            plt.show()
+        kqEst=np.exp(intercept)
+        fac=(np.power(q,alphaEst)-q)/(alphaEst-1)
+        c1Est=kqEst/fac
+        umfParamEsts.append((alphaEst,c1Est))
+    return umfParamEsts
+    
 
-# Uncomment this to apply fractional integration before analysis.
-# This can convert non-conservative to conservative multifractals.
-if len(sys.argv)>2:
-    H=float(sys.argv[2])
-    if np.abs(H) > 1.0e-3:
-        print("Fractional integration, order=",-H)
-        data=fracInt.fractionalIntegration(data,H)
-
-# Fractional integration can lead to negative fluxes
-# Set values below a small threshold to the small threshold value
-data[data<1.0e-6]=1.0e-6
-
-# Determine the number of decomposition levels needed
-n0=data.shape[0]
-n1=data.shape[1]
-nLevs=int(np.floor(np.log2(np.min((n0,n1)))))
-
-# Compute bare fluxes as pyramid
-imPyramid=pyramid(data,nLevs)
-
-minScale=1
-maxScale=5
-D=2.
-nScales=maxScale-minScale+1
-
-# Flux at shortest scale considered
-epsLambda0=imPyramid[minScale]
-lambda0=np.power(2.,maxScale)/np.power(2.,minScale)
-
-etaVals=np.exp(np.linspace(-2.0,1.0,num=31,endpoint=True))
-qVals = [ 1.5, 2.0, 2.5, 3.0 ]
-
-tr=np.empty((len(qVals),len(etaVals),nScales))
-kQEta=np.empty((len(qVals),len(etaVals)))
-
-for (iEta,eta) in enumerate(etaVals):
-    # Dress flux by raising to power eta
-    epsEtaLambda0=np.power(epsLambda0,eta)
-    lambdas=[]
-    imPyramidEta=pyramid(epsEtaLambda0,nScales)
-
-    # Compute trace moments (as in Single Trace Moment method) but on on epsEtas, for a range of q and scale (for this value of eta)
-    for iLvl in range(nScales):
-        lambdaLvl=np.power(2.,maxScale)/np.power(2.,minScale+iLvl)
-        lambdas.append(lambdaLvl)
-        for (iq,q) in enumerate(qVals):
-            dressedFlux=imPyramidEta[iLvl]*np.power(lambda0,D)
-            dressedFlux[dressedFlux<0.0]=0.0 # supress rounding errors that lead to small negative values
-            tr[iq,iEta,iLvl]=np.sum(np.power(dressedFlux,q))
-
-    # Now estimate K(q,eta) from slopes of plots of log(Tr_lambda[epsilon_lamda^q]) against log(lambda)
-    slopes=np.empty((len(qVals),len(etaVals)))
-    for iq,q in enumerate(qVals):
-        (m,c)=np.polyfit(np.log2(lambdas),np.log2(tr[iq,iEta,:]),1)
-        kQEta[iq,iEta]=m+(q-1.)*D
-
-# Evaluate the the slopes of log(K(q,eta)) vs log(eta) for fixed qs to get alpha
-umfParamEsts=[]
-for (iq,q) in enumerate(qVals):
-    (alphaEst,intercept)=np.polyfit(np.log2(etaVals),np.log2(np.absolute(kQEta[iq,:])),1)
-    plt.plot(np.log2(etaVals),np.log2(np.absolute(kQEta[iq,:])))
-    plt.plot(np.log2(etaVals),alphaEst*np.log2(etaVals)+intercept)
-    plt.show()
-    # intercept=log2(k(q))
-    kqEst=np.power(2.,intercept)
-    fac=(np.power(q,alphaEst)-q)/(alphaEst-1)
-    c1Est=kqEst/fac
-    umfParamEsts.append((alphaEst,c1Est))
-print(umfParamEsts)
-
-
+if __name__ == '__main__':
+    
+    # Load data
+    data=np.genfromtxt(sys.argv[1],delimiter=",")#[175:190,290:305]
+    
+    if len(sys.argv)>2:
+        etaMin=float(sys.argv[2])
+        etaMax=float(sys.argv[3])
+    else:
+        etaMin=-2.
+        etaMax=1.
+    
+    # Uncomment this to apply fractional integration before analysis.
+    # This can convert non-conservative to conservative multifractals.
+    if len(sys.argv)>4:
+        H=float(sys.argv[4])
+        if np.abs(H) > 1.0e-3:
+            print("Fractional integration, order=",-H)
+            data=fracInt.fractionalIntegration(data,H)
+    
+    # Fractional integration can lead to negative fluxes
+    # Set values below a small threshold to the small threshold value
+    data[data<1.0e-16]=1.0e-16
+    
+    # Divide by mean to normalize
+    data/=np.mean(data)
+    
+    (alpha,C1)=dtm(data,minScale=1,maxScale=5,etaMin=etaMin,etaMax=etaMax,qVals=[1.5],debug=True)[0]
+    print(alpha,C1)
 
